d1c00a7855de0cad1bbd52b5d65f8197b473a068
nsa12 <singh.navjot12@gmail.com>
2016-08-27 10:18:00 +0530

    should work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn pokemonGo.wsgi
+web: gunicorn PokemonGo.wsgi
--- search/models.py
+++ search/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Pokedex(models.Model):
+class Pokedex(models.Model):			#Used to define structure of database
 	pokemon_name = models.CharField(max_length=128, unique=True)
 	pokemon_type = models.CharField(max_length=128)
 	pokemon_image = models.CharField(max_length=128)
